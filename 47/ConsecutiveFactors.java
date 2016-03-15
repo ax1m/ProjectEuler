07730d59d057b0803c7cbb9d68a1ed6a4244cea0
ax1m <axim92@gmail.com>
2016-03-15 15:59:18 +0100

    Needs rewrite

--- 47/ConsecutiveFactors.java
+++ 47/ConsecutiveFactors.java
@@ -5,11 +5,13 @@ import java.util.Map;
 import java.util.Set;
 
 public class ConsecutiveFactors {
+
+	public static Map<Integer, Map<Integer, Integer>> factorization = new HashMap<Integer, Map<Integer, Integer>>();
+
 	public static void main(String[] args) {
 		int arg;
 		if(args.length == 0) { arg = 4; }
 		else { arg = Integer.parseInt(args[0]); }
-		Map<Integer, Map<Integer, Integer>> factorization = new HashMap<Integer, Map<Integer, Integer>>();
 		boolean resultFound = false;
 		int num = 0;
 		while(!resultFound) {
@@ -34,28 +36,18 @@ public class ConsecutiveFactors {
 	}
 
 	public static Map<Integer, Integer> primeFactors(int number) {
-		Map<Integer, Integer> res = new HashMap<Integer, Integer>();
-		int _number = number;
-		for(int i = 2; i < _number; i++) {
-			int count = 0;
-			while(number % i == 0) {
-				number /= i;
-				count++;
-			}
-			if(count > 0) { res.put(i, count); }
-		}
-		return res;
+		
+	//	This was very wrong. Needs rewrite
+
 	}
 
-	public static String printFactorization(Map<Integer, Integer> fac) {
-		String res = "";
-		if(fac.isEmpty()) { res = "PRIME"; }
-		else {
-			Integer[] keys = fac.keySet().toArray(new Integer[fac.size()]);
-			Arrays.sort(keys);
-			for(int i : keys) { res += i + " ^ " + fac.get(i) + " * "; }
-			res = res.substring(0, res.length() - 3);
-		}
+	public static String printFactorization(int number) {
+		Map<Integer, Integer> fac = factorization.get(number);
+		String res = number + " = ";
+		Integer[] keys = fac.keySet().toArray(new Integer[fac.size()]);
+		Arrays.sort(keys);
+		for(int i : keys) { res += i + " ^ " + fac.get(i) + " * "; }
+		res = res.substring(0, res.length() - 3);
 		return res;
 	}
 }
