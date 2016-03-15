371c364e7b599692eb68f44e03e410c5969f4321
ax1m <axim92@gmail.com>
2016-03-15 17:57:41 +0100

    This looks like it might work but it really doesn't

--- 47/ConsecutiveFactors.java
+++ 47/ConsecutiveFactors.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class ConsecutiveFactors {
 
-	public static Map<Integer, Map<Integer, Integer>> factorization = new HashMap<Integer, Map<Integer, Integer>>();
+	private static Map<Integer, Map<Integer, Integer>> factorMap = new HashMap<Integer, Map<Integer, Integer>>();
 
 	public static void main(String[] args) {
 		int arg;
@@ -16,37 +16,45 @@ public class ConsecutiveFactors {
 		int num = 0;
 		while(!resultFound) {
 			num++;
-			factorization.put(num, primeFactors(num));
-			System.out.println((num) + " = " + printFactorization(factorization.get(num)));
+			factorMap.put(num, primeFactors(num));
+			System.out.println(printFactorization(num));
 			if(num >= arg) {
 				Set<Map<Integer, Integer>> factorizations = new HashSet<Map<Integer, Integer>>();
 				for(int i = 0; i < arg; i++) {
-					factorizations.add(factorization.get(num - i));
+					factorizations.add(factorMap.get(num - i));
 				}
 				boolean unique = false;
 				// TODO -- Add logic
 				if(unique) {
 					resultFound = true;
 					for(int i = num; i < num + arg; i++) {
-						System.out.println(i + " = " + printFactorization(factorization.get(i)));
+						System.out.println(printFactorization(i));
 					}
 				}
 			}
 		}
 	}
 
-	public static Map<Integer, Integer> primeFactors(int number) {
-		
-	//	This was very wrong. Needs rewrite
+	public static Map<Integer, Integer> primeFactors(int num) { return primeFactors(num, 2); }
 
+	public static Map<Integer, Integer> primeFactors(int num, int div) {
+		if(factorMap.containsKey(num)) { return factorMap.get(num); }
+		Map<Integer, Integer> res = new HashMap<Integer, Integer>();
+		if(num < 0) { num *= -1; res.put(-1, 1); }
+		if(num == 0) { res.put(0, 1); return res; }
+		if(num == 1) { return res; }
+		while((div < num) && !(num % div == 0)) { div++; }
+		if(div < num) { num /= div; res = primeFactors(num, div); }
+		res.put(div, res.containsKey(div) ? res.get(div) + 1 : 1);
+		return res;
 	}
 
-	public static String printFactorization(int number) {
-		Map<Integer, Integer> fac = factorization.get(number);
-		String res = number + " = ";
+	public static String printFactorization(int num) {
+		Map<Integer, Integer> fac = factorMap.get(num);
+		String res = num + " = ";
 		Integer[] keys = fac.keySet().toArray(new Integer[fac.size()]);
 		Arrays.sort(keys);
-		for(int i : keys) { res += i + " ^ " + fac.get(i) + " * "; }
+		for(int i : keys) { res += i + ((fac.get(i) > 1) ? " ^ " + fac.get(i) : "") + " * "; }
 		res = res.substring(0, res.length() - 3);
 		return res;
 	}
