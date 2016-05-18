4edcc2090878b11a75472c4fad0b0e9cc7155f64
ax1m <axim92@gmail.com>
2016-05-18 09:30:28 +0200

    Solved problem 33

---
+++ 33/digitCancelling.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// setting type to be stored in the arraylist
+#define T double
+
+// custom C library, found at gist.github.com/ax1m
+#include "arraylist.h"
+
+int main(void) {
+	
+	for(char i=48; i<58; i++) {
+
+	}
+	return 0;
+}
+
+double divide(int x, int y) { return ((double)x / (double)y); }
+
